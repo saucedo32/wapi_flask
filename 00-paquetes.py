37e2bba47edad5ad2ea49268fbd646a30c5a213f
leonardo_saucedo <saucedo32@gmail.com>
2022-10-04 16:01:01 -0300

    .

---
+++ 00-paquetes.py
@@ -0,0 +1,5 @@
+print(22)
+
+pip install python-dotenv
+
+ 
--- app.py
+++ app.py
@@ -1,5 +1,26 @@
 
+
+
+#pip install requirements.txt
+
 print(22)
+import pandas as pd
+
+
+w = [2,3,4]
+w = pd.DataFrame(w)
+print(w.columns)
+print(w[2])
+
+
+#from dotenv import load_dotenv
+#oad_dotenv()
+
+
+
+
+
+
 
 
 
