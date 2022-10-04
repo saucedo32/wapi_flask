a56adf683af526c8463f7132b52e1730636451ce
leonardo_saucedo <saucedo32@gmail.com>
2022-10-04 16:10:19 -0300

    avances

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
 
-
-
+# PRIMEROS PASOS:
 #pip install requirements.txt
+
+
+# IMPORTO LAS LIBRERIAS A USAR AQUI:
+import os
+from dotenv import load_dotenv
+import requests
+import json
+
+
+
+# Cargamos las variables de entorno:
+load_dotenv()
+
+
+TOKEN
+
+
+
+
+
+
+
+print()
+
+
 
 print(22)
 import pandas as pd
 
 
 w = [2,3,4]
-w = pd.DataFrame(w)
-print(w.columns)
-print(w[2])
+print(w)
 
 
 #from dotenv import load_dotenv
